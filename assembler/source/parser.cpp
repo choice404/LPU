c8e0aa7a9dd0ed336267446aade79216e23c386d
choisauce <austinch20@protonmail.com>
2024-02-26 23:22:42 -0800

    Cleaning up the repo a bit

--- assembler/source/parser.cpp
+++ assembler/source/parser.cpp
@@ -1,7 +1,7 @@
 /*
-Copyright (C) 2023 Lysus
-See end of file for extended copyright information
-*/
+ * Copyright (C) 2024 Austin Choi
+ * See end of file for extended copyright information
+ */
 
 // Include header files
 #include <iostream>
@@ -22,12 +22,11 @@ void Parser::parse()
 }
 
 /*
-Copyright (C) 2023 Lysus
-
-cpp_LPUAssembler
-
-Assembler for the LPU V1 CPU Instruction Set Architecture
-
-This code is licensed under the GNU General Public License v3.0 license.
-Please see the LICENSE file in the root directory of this project for the full license details.
-*/
+ * Copyright (C) 2024 Austin Choi
+ * 
+ * lpu
+ * A custom 8 bit pure harvard architecture CPU built in minecraft. This project will consist of the specifications of the CPU as well as an external assembler that will assemble code down to the target machine code as well as a compiler for a custom language called CMine that will compile down to the target assembly code then be assembled into the machine code. This langugae will be a subset of C just to make develping for the CPU easier
+ *
+ * This code is licensed under the GNU General Public License 3.0.
+ * Please see the LICENSE file in the root directory of this project for the full license details.
+ */
